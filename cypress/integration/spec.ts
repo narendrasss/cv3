51e44a47a9bd3b5000008c09cd210a456f10336b
Narendras <n.syahrasyad.n@gmail.com>
2019-07-17 15:51:53 -0700

    Added cypress for testing

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,9 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="Cypress" />
+
+describe('test', () => {
+  it('shows accounts', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
